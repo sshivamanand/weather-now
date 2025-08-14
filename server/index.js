713260ae77c48ea885e1c6e65ab48af0241a02d0
Shivam Anand <shivamanand1635@gmail.com>
2025-08-14 19:39:40 +0530

    fixed db object

--- server/index.js
+++ server/index.js
@@ -11,13 +11,13 @@ import helmet from "helmet";
 
 dotenv.config();
 
-const { Pool } = pg;
 const db = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false } 
 });
 
 const app = express();
